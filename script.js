76fd474cb25590a260b58f12102cd9b215c0f6f5
william-oliveira-sena <92284934+william-oliveira-sena@users.noreply.github.com>
2021-12-31 17:36:12 -0300

    Alteração de teste telefone

---
+++ script.js
@@ -0,0 +1,53 @@
+// Formatação e teste do telefone se celular ou fixo para salvar na formatação correto com traço
+ 
+   function tiraHifen(telefone) {
+    const textoAtual = telefone.value;
+    const textoAjustado = textoAtual.replace(/\-/g, '' );
+
+    telefone.value = textoAjustado;
+    console.log(textoAjustado);
+}
+
+    function mascaraDeTelefone(telefone){
+        const textoAtual = telefone.value;
+        const isCelular = textoAtual.length === 11;
+        let textoAjustado;
+        if(isCelular) {
+            const ddd = textoAtual.slice(0,2);
+            const parte1 = textoAtual.slice(2,7);
+            const parte2 = textoAtual.slice(7,11);
+            textoAjustado = `${ddd}-${parte1}-${parte2}`        
+        } else {
+            const ddd = textoAtual.slice(0,2);
+            const parte1 = textoAtual.slice(2,6);
+            const parte2 = textoAtual.slice(6,10);
+            textoAjustado = `${ddd}-${parte1}-${parte2}`
+        }
+        telefone.value = textoAjustado;
+}
+
+    function cadastrar(){
+
+        totalcad = 0;
+        cadalunos = [];
+
+            let nome = document.getElementById("nome").value;
+            let tel = document.getElementById("telefone").value;
+            let nasc = document.getElementById("nascimento").value;
+            let nota = document.getElementById("nota").value;
+
+        //formatação da data para padrão Brasileiro 
+        data = new Date(nasc);
+        dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+
+        let cad = {nome:nome, telefone:tel, nascimento:dataFormatada, nota:nota};
+        cadalunos[totalcad] = cad;
+           
+
+        let elemento = document.getElementById("saida");
+    
+    
+     elemento.innerHTML += " Nome: " + cadalunos[totalcad].nome + "<br>" + "Telefone:" + cadalunos[totalcad].telefone + "<br>" + "Data de Nascimento:" + cadalunos[totalcad].nascimento + "<br>" + "Nota:" + cadalunos[totalcad].nota + "<br><br>";
+
+    totalcad++;
+}
